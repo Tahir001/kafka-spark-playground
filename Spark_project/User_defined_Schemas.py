36c103e5c1e9c5a4ef4692f71acd3a8c86ec6224
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2024-06-29 20:18:43 -0400

    Defining Schemas in Spark

---
+++ Spark_project/User_defined_Schemas.py
@@ -0,0 +1,29 @@
+# import required libraries
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, IntegerType, FloatType, StringType, StructField
+
+# Build a spark session
+spark = SparkSession.builder.appName("employees").getOrCreate()
+
+# Define the schema 
+# False' indicates null values are NOT allowed for the column.
+schema = StructType([
+    StructField("Emp_Id", StringType(), False),
+    StructField("Emp_Name", StringType(), False),
+    StructField("Department", StringType(), False),
+    StructField("Salary", IntegerType(), False),
+    StructField("Phone", StringType(), True),
+])
+
+#create a dataframe on top a csv file
+df = (spark.read
+    .format("csv")
+    .schema(schema)
+    .option("header", "true")
+    .load("employees.csv")
+)
+# display the dataframe content
+df.show()
+
+# Display the schema 
+df.printSchema()
